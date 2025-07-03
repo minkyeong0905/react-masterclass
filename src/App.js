b609d0da47bbe4d61c9ee3bd149ebccd51fef931
minkyeong0905 <mingit0905@naver.com>
2025-07-03 14:55:13 +0900

    Use as & attrs

--- src/App.js
+++ src/App.js
@@ -4,12 +4,6 @@ const Father = styled.div`
   display: flex;
 `;
 
-const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
-`;
-
 const Input = styled.input.attrs({ required: true })`
   background-color: tomato;
 `;
