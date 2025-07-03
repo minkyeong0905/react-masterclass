7002c9d36464c24b3f60524432a2797cf13abd59
minkyeong0905 <mingit0905@naver.com>
2025-07-03 16:34:22 +0900

    Practice pseudo selectors

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import styled, { keyframes } from "styled-components";
 
 const Wrapper = styled.div`
   display: flex;
+  height: 100vh;
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
 `;
 
 const rotationAnimation = keyframes`
@@ -21,6 +25,10 @@ const rotationAnimation = keyframes`
   }
 `;
 
+const Emoji = styled.span`
+  font-size: 36px;
+`;
+
 const Box = styled.div`
   height: 200px;
   width: 200px;
@@ -30,16 +38,8 @@ const Box = styled.div`
   align-items: center;
   animation: ${rotationAnimation} 1s linear infinite;
 
-  span {
-    font-size: 36px;
-
-    &:hover {
-      font-size: 48px;
-    }
-
-    &:active {
-      opacity: 0;
-    }
+  ${Emoji}:hover {
+      font-size: 98px;
   }
 `;
 
@@ -47,8 +47,9 @@ function App() {
   return (
     <Wrapper>
       <Box>
-        <span>😀</span>
+        <Emoji>😀</Emoji>
       </Box>
+      <Emoji>❤️</Emoji>
     </Wrapper>
   );
 }
