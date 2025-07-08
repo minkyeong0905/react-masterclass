25850b6127ec6700d54e5f275762d044974b34c0
minkyeong0905 <mingit0905@naver.com>
2025-07-08 15:11:28 +0900

    Installed react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,6 @@
-import React from "react";
-import styled from "styled-components";
-
-const Container = styled.div`
-  background-color: ${(props) => props.theme.bgColor};
-`;
-
-const H1 = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`;
-
-interface DummyProps {
-  text: string;
-  active?: boolean;
-}
-
-function Dummy({ text, active = false }: DummyProps) {
-  return <H1>{text}</H1>;
-}
-
 function App() {
-  const onClick = (event: React.FormEvent<HTMLButtonElement>) => {};
-
   return (
-    <Container>
-      <H1>protected</H1>
-      <Dummy text="hello" active={true} />
-      <form>
-        <button onClick={onClick}>Click Me</button>
-      </form>
-    </Container>
+    <div></div>
   );
 }
 
