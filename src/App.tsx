ad2799e8b92ade57dcac0133dac35140bbf9e545
minkyeong0905 <mingit0905@naver.com>
2025-07-08 14:20:43 +0900

    Recap typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
@@ -8,10 +9,25 @@ const H1 = styled.h1`
   color: ${(props) => props.theme.textColor};
 `;
 
+interface DummyProps {
+  text: string;
+  active?: boolean;
+}
+
+function Dummy({ text, active = false }: DummyProps) {
+  return <H1>{text}</H1>;
+}
+
 function App() {
+  const onClick = (event: React.FormEvent<HTMLButtonElement>) => {};
+
   return (
     <Container>
       <H1>protected</H1>
+      <Dummy text="hello" active={true} />
+      <form>
+        <button onClick={onClick}>Click Me</button>
+      </form>
     </Container>
   );
 }
