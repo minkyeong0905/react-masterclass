4ac650702d17ff65f42c211afd3c1bca6dc91f4b
minkyeong0905 <mingit0905@naver.com>
2025-07-15 11:07:38 +0900

    Using api to get coin lists and Updated Coin.tsx (css)

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,8 +1,11 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 import styled from "styled-components";
 
 const Container = styled.div`
     padding: 0px 20px;
+    max-width: 480px;
+    margin: 0 auto;
 `;
 
 const Header = styled.header`
@@ -22,9 +25,10 @@ const Coin = styled.li`
     margin-bottom: 10px;
 
     a {
+        display: flex;
+        align-items: center;
         padding: 20px;
         transition: color 0.2s ease-in;
-        display: block;
     }
 
     &:hover {
@@ -39,48 +43,57 @@ const Title = styled.h1`
     color: ${(props) => props.theme.accentColor};
 `;
 
-const coins = [
-    {
-    id: "btc-bitcoin",
-    name: "Bitcoin",
-    symbol: "BTC",
-    rank: 1,
-    is_new: false,
-    is_active: true,
-    type: "coin",
-    },
-    {
-    id: "eth-ethereum",
-    name: "Ethereum",
-    symbol: "ETH",
-    rank: 2,
-    is_new: false,
-    is_active: true,
-    type: "coin",
-    },
-    {
-    id: "hex-hex",
-    name: "HEX",
-    symbol: "HEX",
-    rank: 3,
-    is_new: false,
-    is_active: true,
-    type: "token",
-    }
-];
+const Loader = styled.span`
+    text-align: center;
+    display: block;
+`;
+
+const Img = styled.img`
+    width: 35px;
+    height: 35px;
+    margin-right: 10px;
+`;
+
+interface CoinInterface {
+    id: string
+   ,name: string
+   ,symbol: string
+   ,rank: number
+   ,is_new: boolean
+   ,is_active: boolean
+   ,type: string
+}
 
 function Coins() {
+    const [coins, setCoins] = useState<CoinInterface[]>([]);
+    const [loading, setLoading] = useState(true);
+    const { state } = useLocation() as { state: { name: string } };
+
+    useEffect(() => {
+        (async() => {
+            const response = await fetch("https://api.coinpaprika.com/v1/coins");
+            const json = await response.json();
+            setCoins(json.slice(0, 100));
+            setLoading(false);
+        })();
+    }, []);
+
     return (
         <Container>
             <Header>
                 <Title>코인</Title>
             </Header>
+            {loading ?
+            (<Loader>Loading...</Loader>) : (
             <CoinsList>
                 {coins.map((coin) => (
                     <Coin key={coin.id}>
-                        <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
+                        <Link to={`/${coin.id}`} state={ coin.name } >
+                            <Img src={`https://cryptocurrencyliveprices.com/img/${coin.id}.png`} />
+                            {coin.name} &rarr;
+                        </Link>
                     </Coin>))}
-            </CoinsList>
+            </CoinsList>)}
         </Container>
     );
 }
