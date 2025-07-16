e4138043d5048e7956104c1bcbfeb5adb194f01a
minkyeong0905 <mingit0905@naver.com>
2025-07-16 10:46:36 +0900

    Added style on Coin.tsx

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -2,6 +2,11 @@ import { useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router-dom";
 import styled from "styled-components";
 
+const Loader = styled.span`
+    text-align: center;
+    display: block;
+`;
+
 const Container = styled.div`
     padding: 0px 20px;
     max-width: 480px;
@@ -20,9 +25,29 @@ const Title = styled.h1`
     color: ${(props) => props.theme.accentColor};
 `;
 
-const Loader = styled.span`
-    text-align: center;
-    display: block;
+const Overview = styled.div`
+    display: flex;
+    justify-content: space-between;
+    background-color: rgba(0, 0, 0, 0.5);
+    padding: 10px 20px;
+    border-radius: 10px;
+`;
+
+const OverviewItem = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    span:first-child {
+        font-size: 10px;
+        font-weight: 400;
+        text-transform: uppercase;
+        margin-bottom: 5px;
+    }
+`;
+
+const Description = styled.p`
+    margin: 20px 0px;
 `;
 
 interface RouteParams {
@@ -118,16 +143,45 @@ function Coin() {
             
             setInfo(infoData);
             setPriceInfo(priceData);
+            setLoading(false);
         })();
-    }, []);
+    }, [coinId]);
     
     return (
         <Container>
             <Header>
-                <Title>{state === null ? "Loading.." : state}</Title>
+                <Title>{state !== null ? state : loading ? "Loading..." : info?.name}</Title>
             </Header>
             {loading ?
-            (<Loader>Loading...</Loader>) : null}
+            (<Loader>Loading...</Loader>) : (
+                <>
+                    <Overview>
+                        <OverviewItem>
+                            <span>Rank :</span>
+                            <span>{info?.rank}</span>
+                        </OverviewItem>
+                        <OverviewItem>
+                            <span>Symbol :</span>
+                            <span>{info?.symbol}</span>
+                        </OverviewItem>
+                        <OverviewItem>
+                            <span>Open Source :</span>
+                            <span>{info?.open_source ? "Yes" : "No"}</span>
+                        </OverviewItem>
+                    </Overview>
+                    <Description>{info?.description}</Description>
+                    <Overview>
+                        <OverviewItem>
+                            <span>Total Supply :</span>
+                            <span>{priceInfo?.total_supply}</span>
+                        </OverviewItem>
+                        <OverviewItem>
+                            <span>Max Supply :</span>
+                            <span>{priceInfo?.max_supply}</span>
+                        </OverviewItem>
+                    </Overview>
+                </>
+            )}
         </Container>
     );
 }
