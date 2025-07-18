1883c3596423b1516e874c69a47f86f212949428
minkyeong0905 <mingit0905@naver.com>
2025-07-18 11:02:46 +0900

    Using useMatch

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Outlet, useLocation, useParams } from "react-router-dom";
+import { Link, Outlet, useLocation, useMatch, useParams } from "react-router-dom";
 import styled from "styled-components";
 
 const Loader = styled.span`
@@ -48,6 +48,28 @@ const OverviewItem = styled.div`
 
 const Description = styled.p`
     margin: 20px 0px;
+`;
+
+const Tabs = styled.div`
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    margin: 25px 0px;
+    gap: 10px;
+`;
+
+const Tab = styled.span<{ isActive: boolean }>`
+    text-align: center;
+    text-transform: uppercase;
+    font-size: 12px;
+    font-weight: 400;
+    background-color: rgba(0, 0, 0, 0.5);
+    padding: 7px 0px;
+    border-radius: 10px;
+    color: ${(props) => props.isActive ? props.theme.accentColor : props.theme.textColor};
+
+    a {
+        display: block;
+    }
 `;
 
 interface RouteParams {
@@ -135,6 +157,8 @@ function Coin() {
     const { state } = useLocation() as ILocation;
     const [info, setInfo] = useState<IInfoData>();
     const [priceInfo, setPriceInfo] = useState<IPriceData>();
+    const priceMatch = useMatch("/:coinId/price");
+    const chartMatch = useMatch("/:coinId/chart");
 
     useEffect(() => {
         (async () => {
@@ -180,6 +204,15 @@ function Coin() {
                             <span>{priceInfo?.max_supply}</span>
                         </OverviewItem>
                     </Overview>
+
+                    <Tabs>
+                        <Tab isActive={priceMatch !== null}>
+                            <Link to={`/${coinId}/price`}>Price</Link>
+                        </Tab>
+                        <Tab isActive={chartMatch !== null}>
+                            <Link to={`/${coinId}/chart`}>Chart</Link>
+                        </Tab>
+                    </Tabs>
 
                     <Outlet />
                 </>
