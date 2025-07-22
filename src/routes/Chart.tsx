ddcbb2ad87676d1d2eaab7fc7e3ceb5625a83c83
minkyeong0905 <mingit0905@naver.com>
2025-07-22 16:07:37 +0900

    Added interface of coin history data

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -2,10 +2,21 @@ import { useQuery } from "react-query";
 import { fetchCoinHistory } from "../api";
 import { useParams } from "react-router-dom";
 
+interface IHistorical {
+    time_open: number;
+    time_close: number;
+    open: string;
+    high: string;
+    low: string;
+    close: string;
+    volume: string;
+    market_cap: number;
+}
+
 function Chart() {
     const { coinId } = useParams();
-    const { isLoading, data } = useQuery(["ohlcv", coinId], () => fetchCoinHistory(coinId!));
-    
+    const { isLoading, data } = useQuery<IHistorical[]>(["ohlcv", coinId], () => fetchCoinHistory(coinId!));
+
     return <h1>Chart</h1>;
 }
 
