05ac90c39053b061a459722c6b5b6dab52e0f261
minkyeong0905 <mingit0905@naver.com>
2025-07-25 15:10:53 +0900

    Updated Coin.tsx (Added home button)

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -28,6 +28,18 @@ const Title = styled.h1`
     color: ${(props) => props.theme.accentColor};
 `;
 
+const Button = styled.div`
+    background-color: rgba(0, 0, 0, 0.2);
+    width: 48px;
+    height: 48px;
+    margin-right: 20px;
+
+    a {
+        display: flex;
+        align-items: center;
+    }
+`;
+
 const Overview = styled.div`
     display: flex;
     justify-content: space-between;
@@ -185,6 +197,13 @@ function Coin() {
                 <title>{state !== null ? state : loading ? "Loading..." : infoData?.name}</title>
             </Helmet>
             <Header>
+                <Button>
+                    <Link to={"/"}>
+                        <svg data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"></path>
+                        </svg>
+                    </Link>
+                </Button>
                 <Title>{state !== null ? state : loading ? "Loading..." : infoData?.name}</Title>
             </Header>
             {loading ?
